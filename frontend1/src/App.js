4e30fb09239b0b5168d595b797b668851bddd7f1
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-09-25 23:06:57 -0400

    Very Basic HomePage and Navbar

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -2,12 +2,13 @@ import Welcome from './Welcome';
 import WelcomeBack from './WelcomeBack';
 import './style.css';
 import Navbar from './components/Navbar';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <WelcomeBack></WelcomeBack>
+      <HomePage></HomePage>
 
     </div>
   );
