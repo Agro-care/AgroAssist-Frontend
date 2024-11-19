0b03b844190c6e8a311f19e0c9cfeb727836ce49
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2024-11-19 15:43:41 -0500

    FIxed the spelling mistake

--- src/components/Homepage/AboutUsSection.js
+++ src/components/Homepage/AboutUsSection.js
@@ -8,7 +8,7 @@ const AboutUsSection = () => {
       <div className="container">
         <h2>About AgroAssist</h2>
         <p>
-          AgriApp is dedicated to empowering farmers by providing cutting-edge tools, resources, 
+          Agro Assist App is dedicated to empowering farmers by providing cutting-edge tools, resources, 
           and knowledge that enhance agricultural productivity. Our mission is to bridge the gap 
           between traditional farming practices and modern technological innovations, helping farmers 
           make informed decisions and achieve sustainable growth.
